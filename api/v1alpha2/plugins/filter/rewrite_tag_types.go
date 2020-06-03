ccc20a920b8d1fbcda2928bd3982b61340cff327
sungil <si.im@sk.com>
2020-06-03 12:06:12 +0000

    auto-gen crd to add "rewirte_tag" filter

--- api/v1alpha2/plugins/filter/rewrite_tag_types.go
+++ api/v1alpha2/plugins/filter/rewrite_tag_types.go
@@ -6,16 +6,16 @@ import (
 
 // +kubebuilder:object:generate:=true
 
-// The RewriteTag Filter plugin allows you to operate on or with nested data. Its modes of operation are
+// The rewrite_tag filter, allows to re-emit a record under a new Tag. Once a record has been re-emitted, the original record can be preserved or discarded.
 type RewriteTag struct {
 	// Defines the matching criteria and the format of the Tag for the matching record.
 	Rules []RewriteTagRule `json:"rules,omitempty"`
 	// When the filter emits a record under the new Tag, there is an internal emitter plugin that takes care of the job.
 	EmitterName string `json:"emitterName,omitempty"`
-	// Define a buffering mechanism for the new records created. Note these records are part of the emitter plugin. 
+	// Define a buffering mechanism for the new records created. Note these records are part of the emitter plugin.
 	// This option support the values memory (default) or filesystem.
 	// +kubebuilder:validation:Enum:=memory;filesystem
-	EmitterStorageType string `json:"emitterStorageType,omitempty"`
+	EmitterStorageType string `json:"emitterStorageType,optional"`
 }
 
 // +kubebuilder:object:generate:=true
@@ -29,7 +29,8 @@ type RewriteTagRule struct {
 	// If a regular expression has matched the value of the defined key in the rule, we are ready to compose a new Tag for that specific record.
 	NewTag string `json:"newTag,omitempty"`
 	// If a rule matches the criteria the filter will emit a copy of the record with the new defined Tag.
-	Keep bool `json:"keep,omitempty"`
+	// +kubebuilder:validation:Enum:=true;false
+	Keep string `json:"keep,omitempty"`
 }
 
 func (_ *RewriteTag) Name() string {
@@ -42,10 +43,10 @@ func (n *RewriteTag) Params(_ plugins.SecretLoader) (*plugins.KVs, error) {
 		kvs.Insert("Rules", rule.Key+" "+rule.Regex+" "+rule.NewTag+" "+rule.Keep)
 	}
 	if n.EmitterName != "" {
-		kvs.Insert("Emitter_Name", n.Operation)
+		kvs.Insert("Emitter_Name", n.EmitterName)
 	}
 	if n.EmitterStorageType != "" {
-		kvs.Insert("Emitter_Storage.type", n.RemovePrefix)
+		kvs.Insert("Emitter_Storage.type", n.EmitterStorageType)
 	}
 	return kvs, nil
 }
